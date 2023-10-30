afcf54ac811f91a63f9d83c74c474c7718cb92be
yo-404 <yogeshy2801@gmail.com>
2023-10-30 12:45:56 +0530

    Feat: Add venue,score and opponent (international)
    Added the below meta info in scrapping for internation fixtures -
    Added match location
    Added Target score and Projected Score by Teams
    Added Opponent Team name

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -2,12 +2,13 @@ package crawler
 
 import "fmt"
 
-func ScrapforTeam() string {
+func ScrapforTeam() (string, string) {
 	baseURL := "https://www.espncricinfo.com/team/"
 	varURL := "/match-schedule-fixtures-and-results"
 	var Team string
 	var choice int
 	var mainURl string
+	var Teamname string
 	fmt.Println("Select Team for internation Team fixtures")
 	fmt.Println("1. INDIA")
 	fmt.Println("2. AUSTRALIA")
@@ -36,44 +37,64 @@ func ScrapforTeam() string {
 	switch choice {
 	case 1:
 		Team = "india-6"
+		Teamname = "India"
 	case 2:
 		Team = "australia-2"
+		Teamname = "Australia"
 	case 3:
 		Team = "england-1"
+		Teamname = "England"
 	case 4:
 		Team = "afghanistan-40"
+		Teamname = "Afghanistan"
 	case 5:
 		Team = "ireland-29"
+		Teamname = "Ireland"
 	case 6:
 		Team = "bangladesh-25"
+		Teamname = "Bangladesh"
 	case 7:
 		Team = "new-zealand-5"
+		Teamname = "New Zealand"
 	case 8:
 		Team = "pakistan-7"
+		Teamname = "Pakistan"
 	case 9:
 		Team = "south-africa-3"
+		Teamname = "South Africa"
 	case 10:
 		Team = "sri-lanka-8"
+		Teamname = "Sri Lanka"
 	case 11:
 		Team = "west-indies-4"
+		Teamname = "West Indies"
 	case 12:
 		Team = "zimbabwe-9"
+		Teamname = "Zimbabwe"
 	case 13:
 		Team = "namibia-28"
+		Teamname = "Namibia"
 	case 14:
 		Team = "nepal-33"
+		Teamname = "Nepal"
 	case 15:
 		Team = "netherlands-15"
+		Teamname = "Netherlands"
 	case 16:
 		Team = "oman-37"
+		Teamname = "Oman"
 	case 17:
 		Team = "papua-new-guinea-20"
+		Teamname = "Papua New Guinea"
 	case 18:
 		Team = "scotland-30"
+		Teamname = "Scotland"
 	case 19:
 		Team = "united-arab-emirates-27"
+		Teamname = "United Arab Emirates"
 	case 20:
 		Team = "united-states-of-america-11"
+		Teamname = "United States Of America"
 
 	default:
 		fmt.Println("Please select a valid choice!!")
@@ -81,5 +102,5 @@ func ScrapforTeam() string {
 	}
 	mainURl = baseURL + Team + varURL
 	fmt.Println(mainURl)
-	return mainURl
+	return mainURl, Teamname
 }
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/yo-404/crickscrapper/crawler"
@@ -13,12 +14,14 @@ import (
 )
 
 type Matches struct {
-	Type       string `json:"type"`
+	Type       string `json:"match"`
 	Tournament string `json:"tournament"`
-	// Location string `json:"location"`
-	Date   string `json:"date"`
-	Result string `json:"result"`
-	Time   string `json:"time"`
+	Location   string `json:"location"`
+	Date       string `json:"date"`
+	Result     string `json:"result"`
+	Score      string `json:"score"`
+	Time       string `json:"time"`
+	Opponent   string `json:"opponent"`
 }
 
 func main() {
@@ -27,19 +30,40 @@ func main() {
 
 	matches := []Matches{}
 
-	CrawlURL := crawler.ScrapforTeam()
+	CrawlURL, Teamname := crawler.ScrapforTeam()
 
 	c.OnHTML("div[class=ds-flex]", func(h *colly.HTMLElement) {
+		team1 := (h.ChildText("div.ds-flex.ds-items-center.ds-min-w-0.ds-mr-1"))
+		team2 := strings.ReplaceAll(team1, Teamname, "")
+		result := h.ChildText("span.ds-text-tight-xs")
+		if result == "RESULT" {
+			result = "RESULT DECLARED"
+		}
+
+		// location
+		var result1 string
+		location := h.ChildText("div.ds-text-tight-s.ds-font-regular.ds-truncate.ds-text-typo-mid3")
+		parts := strings.Split(location, "•")
+		if len(parts) > 1 {
+			result1 = strings.TrimSpace(parts[1])
+		}
+		for i := 0; i < len(result1); i++ {
+			if result1[i] == ',' {
+				location = result1[0:i]
+				break
+			}
+
+		}
+
 		i := Matches{
 			Date:       h.ChildText("div.ds-text-compact-xs.ds-font-bold.ds-w-24"),
 			Type:       h.ChildText("span.ds-text-tight-s.ds-font-medium.ds-text-typo"),
 			Tournament: h.ChildText("span.ds-text-tight-s.ds-font-regular.ds-text-typo.ds-underline.ds-decoration-ui-stroke"),
+			Location:   location,
 			Result:     h.ChildText("p.ds-text-tight-s.ds-font-regular.ds-line-clamp-2.ds-text-typo"),
-			Time:       h.ChildText("span.ds-text-tight-xs"),
-
-			// fmt.Println(Time)
-			// Match := h.ChildAttr("span[]", "title")
-			// fmt.Println(Match)
+			Score:      h.ChildText("div.ds-text-compact-s.ds-text-typo.ds-text-right.ds-whitespace-nowrap"),
+			Time:       result,
+			Opponent:   team2,
 		}
 
 		matches = append(matches, i)
@@ -47,17 +71,30 @@ func main() {
 	})
 
 	c.OnHTML("div[class=ds-flex]", func(h *colly.HTMLElement) {
-		// text := h.ChildText("div.ds-flex.div[class=ds-text-compact-xs.ds-font-bold.ds-w-24]")
-		// fmt.Println(h.Text)
-		// text := h.ChildText("div[class=ds-text-compact-xs ds-font-bold ds-w-24]")
-		// fmt.Println(text)
-		// date := h.ChildText("div.ds-text-compact-xs.ds-font-bold.ds-w-24")
-		// fmt.Println(date)
-		// match := h.ChildText()
-		// fmt.Println(match)
-		// Test := h.ChildAttr("a.ds-inline-flex.ds-items-start.ds-leading-none.!ds-inline", "title")
-		// fmt.Println(Test)
+		// team1 := h.ChildAttr("div.ds-flex.ds-items-center.ds-min-w-0.ds-mr-1", "title") != "India"
+		// if h.ChildText("div.ds-flex.ds-items-center.ds-min-w-0.ds-mr-1") != "India" && h.ChildText("div.ds-flex.ds-items-center.ds-min-w-0.ds-mr-1") != "IND" {
+		// 	team1 := (h.ChildText("div.ds-flex.ds-items-center.ds-min-w-0.ds-mr-1"))
+		// 	team2 := strings.ReplaceAll(team1, "India", "")
+		// 	fmt.Println(team2)
+		// var result string
+		// location := h.ChildText("div.ds-text-tight-s.ds-font-regular.ds-truncate.ds-text-typo-mid3")
+		// parts := strings.Split(location, "•")
+		// if len(parts) > 1 {
+		// 	result = strings.TrimSpace(parts[1])
+		// 	// fmt.Println(result)
+		// }
+		// for i := 0; i < len(result); i++ {
+		// 	if result[i] == ',' {
+		// 		fmt.Println(result[0:i])
+		// 		break
+		// 	}
+
+		// }
+		// score := h.ChildText("div.ds-text-compact-s.ds-text-typo.ds-text-right.ds-whitespace-nowrap")
+		// fmt.Println(score)
 	})
+
+	// fmt.Println(Test)
 
 	// c.OnHTML("div[class=ds-flex]", func(h *colly.HTMLElement) {
 	// test := h.ChildText("div.ds-text-tight-s.ds-font-regular.ds-truncate.ds-text-typo-mid3#text")
