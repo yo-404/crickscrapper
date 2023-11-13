e4a0ea8914b30fb0c06e4dce9a3408483bfa27ec
yo-404 <yogeshy2801@gmail.com>
2023-11-13 20:54:48 +0530

    chore: change destination of output files

--- fixtures/savejson.go
+++ fixtures/savejson.go
@@ -2,22 +2,33 @@ package Fixture
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strings"
 )
 
 func SaveToJson(data []byte) {
+
+	if _, err := os.Stat("output"); os.IsNotExist(err) {
+		err := os.MkdirAll("output", 0777)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Directory created: output \n")
+	}
+
 	// var choice string
 	var filename string
 	fmt.Println("Please enter the file name ")
 	fmt.Scan(&filename)
-	prefix := "./"
+	prefix := "./output/"
 	file := prefix + filename
 	suffix := ".json"
 	filename = HasSuffix(file, suffix)
-	file1 := strings.TrimPrefix(filename, "./")
+	file1 := strings.TrimPrefix(filename, "./output/")
+
 	err := os.WriteFile(filename, data, 0644)
-	fmt.Printf("File Successfully saved in the ./ directory as %v \n \n ", file1)
+	fmt.Printf("File Successfully saved in the output directory as %v \n \n ", file1)
 	if err != nil {
 		panic(err)
 	}
